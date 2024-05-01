a5e26dda26e2f5f2d538d46b2adabb3890dcc7ad
wangchong <2606585912@qq.com>
2024-05-01 16:02:09 +0800

    起始工程搭建

---
+++ ems-parent/ems-base/src/main/java/org/letian/App.java
@@ -0,0 +1,13 @@
+package org.letian;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
